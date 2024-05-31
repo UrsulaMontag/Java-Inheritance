08e9aec67ecfb702f324792f1ef575fb53cf134f
ugla <montagu666@gmail.com>
2024-05-31 12:28:04 +0200

    Add motorcycle print method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,8 +6,10 @@ public class Main {
         // classes and call the methods to output the information.
         Vehicle myVehicle = new Vehicle("Company", "Car", 2000);
         Car myCar = new Car("Toyota", "Corolla", 2000, 2);
+        MotorCycle myMotorCycle = new MotorCycle("Sauberwisch", "Feuerblitz", 2012, "flying broom", 300, 50);
 
         myVehicle.printVehicle();
         myCar.printVehicle();
+        myMotorCycle.printVehicle();
     }
 }
--- src/main/java/org/example/MotorCycle.java
+++ src/main/java/org/example/MotorCycle.java
@@ -3,12 +3,27 @@ package org.example;
 public class MotorCycle extends Vehicle {
     // Create another derived class from "Vehicle" called "Motorcycle". Add appropriate properties to this class.
 
+    private String cycleType;
     private int speed;
     private int acceleration;
 
-    public MotorCycle(String manufacturer, String model, int yearOfManufacture, int speed, int acceleration) {
+    public MotorCycle(String manufacturer, String model, int yearOfManufacture, String cycleType, int speed, int acceleration) {
         super(manufacturer, model, yearOfManufacture);
+        this.cycleType = cycleType;
         this.speed = speed;
         this.acceleration = acceleration;
     }
+
+    // Implement a method in the "Motorcycle" class that returns the
+    // type of motorcycle (e.g. Chopper, Sportbike, Cruiser).
+    public String getCycleType() {
+        return cycleType;
+    }
+
+    @Override
+    public void printVehicle() {
+        super.printVehicle();
+        System.out.println("Your Motorcycle is a: " + cycleType + " " + speed + " " + acceleration);
+    }
+
 }
