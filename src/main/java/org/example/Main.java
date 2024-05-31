7327ef7fda95a9c84c71bb1db28ec0e98097837d
ugla <montagu666@gmail.com>
2024-05-31 12:06:27 +0200

    Instance car and vehicle and call method print

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        // Step 5: Create instances of the "Vehicle" and "Car"
+        // classes and call the methods to output the information.
+        Vehicle myVehicle = new Vehicle("Company", "Car", 2000);
         Car myCar = new Car("Toyota", "Corolla", 2000, 2);
+
+        myVehicle.printVehicle();
         myCar.printVehicle();
     }
 }
